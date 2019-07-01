1b1e48bd1e55c15e54107f4539fc19a559a5baad
Georgy Manasyan <georgy.manasyan@gmail.com>
2019-07-01 17:34:20 +0300

    HW04 Base, Optional Done.

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -11,6 +11,7 @@ public abstract class AbstractBaseEntity {
     @Id
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+    @Column(name = "id", nullable = false)
     protected Integer id;
 
     protected AbstractBaseEntity() {
--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -5,10 +5,13 @@ import ru.javawebinar.topjava.model.Meal;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.time.LocalDateTime.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
 
 public class MealTestData {
     public static final int MEAL1_ID = START_SEQ + 2;
@@ -25,11 +28,19 @@ public class MealTestData {
 
     public static final List<Meal> MEALS = Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
 
+    static {
+        MEALS.forEach(meal -> meal.setUser(USER) );
+
+        ADMIN_MEAL1.setUser(ADMIN);
+        ADMIN_MEAL2.setUser(ADMIN);
+    }
+
     public static Meal getCreated() {
         return new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Созданный ужин", 300);
     }
 
     public static Meal getUpdated() {
+
         return new Meal(MEAL1_ID, MEAL1.getDateTime(), "Обновленный завтрак", 200);
     }
 
